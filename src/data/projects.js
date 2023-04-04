c0b1b7e4fc1a62ea2ae444d4b0991dd020325820
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-04-04 08:52:15 +0430

    created projects data

---
+++ src/data/projects.js
@@ -0,0 +1,129 @@
+import logo_motion_1 from '../images/projects/logo-motion-1.jpg';
+
+import edit_1 from '../images/projects/edit/edit-1.jpg';
+import edit_2 from '../images/projects/edit/edit-2.jpg';
+import edit_3 from '../images/projects/edit/edit-3.jpg';
+import edit_4 from '../images/projects/edit/edit-4.jpg';
+
+import mazhabi_1 from '../images/projects/mazhabi/mazhabi-1.jpg';
+import mazhabi_2 from '../images/projects/mazhabi/mazhabi-2.jpg';
+import mazhabi_3 from '../images/projects/mazhabi/mazhabi-3.jpg';
+import mazhabi_4 from '../images/projects/mazhabi/mazhabi-4.jpg';
+import mazhabi_5 from '../images/projects/mazhabi/mazhabi-5.jpg';
+import mazhabi_6 from '../images/projects/mazhabi/mazhabi-6.jpg';
+
+export const projects = {
+	'موشن گرافیک': [
+		{
+			id: 1,
+			name: 'لوگو موشن مجموعه اسطوره',
+			slug: 'logo-motion-1',
+			posterSrc: logo_motion_1,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+		{
+			id: 2,
+			name: 'Jordan',
+			slug: 'logo-motion-2',
+			posterSrc: logo_motion_1,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+		{
+			id: 3,
+			name: 'Skacher',
+			slug: 'logo-motion-3',
+			posterSrc: logo_motion_1,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+		{
+			id: 4,
+			name: 'Reebok',
+			slug: 'logo-motion-4',
+			posterSrc: logo_motion_1,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+		{
+			id: 5,
+			name: 'Adidas',
+			slug: 'logo-motion-5',
+			posterSrc: logo_motion_1,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+	],
+
+	تدوین: [
+		{
+			id: 1,
+			name: 'زمانت بسیار کمه',
+			slug: 'edit-1',
+			posterSrc: edit_1,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+		{
+			id: 2,
+			name: 'سختی رو زندگی کن',
+			slug: 'edit-2',
+			posterSrc: edit_2,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+		{
+			id: 3,
+			name: 'برشی کوتاه از زندگیم',
+			slug: 'edit-3',
+			posterSrc: edit_3,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+		{
+			id: 4,
+			name: 'ترس معنای واقعی قدرته',
+			slug: 'edit-4',
+			posterSrc: edit_4,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+	],
+
+	طراحی: [
+		{
+			id: 1,
+			name: 'طراحی بک گراند شهادت امام کاظم (ع)',
+			slug: 'mazhabi-1',
+			posterSrc: mazhabi_1,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+		{
+			id: 2,
+			name: 'طراحی پوستر ایام فاطمیه',
+			slug: 'mazhabi-2',
+			posterSrc: mazhabi_2,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+		{
+			id: 3,
+			name: 'طراحی کاور پست',
+			slug: 'mazhabi-3',
+			posterSrc: mazhabi_3,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+		{
+			id: 4,
+			name: 'طراحی پوستر هیئت هفتگی',
+			slug: 'mazhabi-4',
+			posterSrc: mazhabi_4,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+		{
+			id: 5,
+			name: 'طراحی پست',
+			slug: 'mazhabi-5',
+			posterSrc: mazhabi_5,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+		{
+			id: 6,
+			name: 'طراحی بک گراند ولادت امام زمان (عج)',
+			slug: 'mazhabi-6',
+			posterSrc: mazhabi_6,
+			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+		},
+	],
+};

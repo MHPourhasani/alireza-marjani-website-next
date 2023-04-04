7ca0e5bd9735587e00076dd2f1d7f0695abb8a90
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-04-04 06:18:29 +0430

    install tailwindcss and added custom styles

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		fontFamily: {
+			iranyekan: ['iranyekan'],
+		},
+		extend: {
+			colors: {
+				bgColor: '#ECF0F3',
+				darkBgColor: '#21252A',
+				primary: '#FD014F',
+				secondary: '#838485',
+				gradientOne: '#EFF3F6',
+				gradientTwo: '#ffffff',
+				darkGradientOne: '#161A20',
+				darkGradientTwo: '#23272C',
+			},
+		},
+	},
+	plugins: [],
+};

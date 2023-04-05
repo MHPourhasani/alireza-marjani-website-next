18d38f5c5067b182a856aa90e63d343cc3faf8f0
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-04-05 07:33:32 +0430

    changed path images and updated projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,27 +1,27 @@
-import logo_motion_1 from '../images/projects/logo-motion-1.jpg';
-import motion_graphic_1 from '../images/projects/motion-graphic-1.jpg';
-import vfx_1 from '../images/projects/vfx-1.jpg';
-import stop_motion_1 from '../images/projects/stop-motion-1.jpg';
-import story_motion_1 from '../images/projects/story-motion-1.jpg';
-import story_motion_2 from '../images/projects/story-motion-2.jpg';
+import logo_motion_1 from '../images/projects/motion graphic/logo-motion-1.jpg';
+import motion_graphic_1 from '../images/projects/motion graphic/motion-graphic-1.jpg';
+import vfx_1 from '../images/projects/motion graphic/vfx-1.jpg';
+import stop_motion_1 from '../images/projects/motion graphic/stop-motion-1.jpg';
+import story_motion_1 from '../images/projects/motion graphic/story-motion-1.jpg';
+import story_motion_2 from '../images/projects/motion graphic/story-motion-2.jpg';
 
 import edit_1 from '../images/projects/edit/edit-1.jpg';
 import edit_2 from '../images/projects/edit/edit-2.jpg';
 import edit_3 from '../images/projects/edit/edit-3.jpg';
 import edit_4 from '../images/projects/edit/edit-4.jpg';
 
-import poster_1 from '../images/projects/poster-1.jpg';
-import poster_2 from '../images/projects/poster-2.jpg';
-import poster_3 from '../images/projects/poster-3.jpg';
-import poster_4 from '../images/projects/poster-4.jpg';
-import mazhabi_1 from '../images/projects/mazhabi/mazhabi-1.jpg';
-import mazhabi_2 from '../images/projects/mazhabi/mazhabi-2.jpg';
-import mazhabi_3 from '../images/projects/mazhabi/mazhabi-3.jpg';
-import mazhabi_4 from '../images/projects/mazhabi/mazhabi-4.jpg';
-import mazhabi_5 from '../images/projects/mazhabi/mazhabi-5.jpg';
-import mazhabi_6 from '../images/projects/mazhabi/mazhabi-6.jpg';
-import logo_1 from '../images/projects/logo-1.jpg';
-import artistic_photo_1 from '../images/projects/artistic-photo-1.jpg';
+import poster_1 from '../images/projects/design/poster-1.jpg';
+import poster_2 from '../images/projects/design/poster-2.jpg';
+import poster_3 from '../images/projects/design/poster-3.jpg';
+import poster_4 from '../images/projects/design/poster-4.jpg';
+import mazhabi_1 from '../images/projects/design/mazhabi-1.jpg';
+import mazhabi_2 from '../images/projects/design/mazhabi-2.jpg';
+import mazhabi_3 from '../images/projects/design/mazhabi-3.jpg';
+import mazhabi_4 from '../images/projects/design/mazhabi-4.jpg';
+import mazhabi_5 from '../images/projects/design/mazhabi-5.jpg';
+import mazhabi_6 from '../images/projects/design/mazhabi-6.jpg';
+import logo_1 from '../images/projects/design/logo-1.jpg';
+import artistic_photo_1 from '../images/projects/design/artistic-photo-1.jpg';
 
 export const projects = {
 	'موشن گرافیک': [
@@ -29,6 +29,7 @@ export const projects = {
 			id: 1,
 			name: 'لوگو موشن مجموعه اسطوره',
 			slug: 'logo-motion-1',
+			category: 'motion-graphic',
 			posterSrc: logo_motion_1,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -36,6 +37,7 @@ export const projects = {
 			id: 2,
 			name: 'لوگوموشن برند Google',
 			slug: 'motion-graphic-1',
+			category: 'motion-graphic',
 			posterSrc: motion_graphic_1,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -43,6 +45,7 @@ export const projects = {
 			id: 3,
 			name: '3D mobile',
 			slug: 'vfx-3',
+			category: 'motion-graphic',
 			posterSrc: vfx_1,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -50,6 +53,7 @@ export const projects = {
 			id: 4,
 			name: 'استاپ موشن ربات',
 			slug: 'stop-motion-4',
+			category: 'motion-graphic',
 			posterSrc: stop_motion_1,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -57,6 +61,7 @@ export const projects = {
 			id: 5,
 			name: 'استوری موشن دوره سیاه قلم',
 			slug: 'story-motion-1',
+			category: 'motion graphic',
 			posterSrc: story_motion_1,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -64,6 +69,7 @@ export const projects = {
 			id: 6,
 			name: 'استوری موشن پادگان',
 			slug: 'story-motion-2',
+			category: 'motion-graphic',
 			posterSrc: story_motion_2,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -75,6 +81,7 @@ export const projects = {
 			name: 'زمانت بسیار کمه',
 			slug: 'edit-1',
 			posterSrc: edit_1,
+			category: 'edit',
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
 		{
@@ -82,12 +89,14 @@ export const projects = {
 			name: 'سختی رو زندگی کن',
 			slug: 'edit-2',
 			posterSrc: edit_2,
+			category: 'edit',
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
 		{
 			id: 3,
 			name: 'برشی کوتاه از زندگیم',
 			slug: 'edit-3',
+			category: 'edit',
 			posterSrc: edit_3,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -95,6 +104,7 @@ export const projects = {
 			id: 4,
 			name: 'ترس معنای واقعی قدرته',
 			slug: 'edit-4',
+			category: 'edit',
 			posterSrc: edit_4,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -105,13 +115,15 @@ export const projects = {
 			id: 1,
 			name: 'طراحی پوستر پادگان',
 			slug: 'poster-1',
+			category: 'design',
 			posterSrc: poster_1,
-			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
+			videoSrc: '',
 		},
 		{
 			id: 1,
 			name: 'طراحی پست اینستاگرام',
 			slug: 'poster-2',
+			category: 'design',
 			posterSrc: poster_2,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -119,6 +131,7 @@ export const projects = {
 			id: 1,
 			name: 'طراحی پوستر فروشگاه send shoes',
 			slug: 'poster-3',
+			category: 'design',
 			posterSrc: poster_3,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -126,6 +139,7 @@ export const projects = {
 			id: 1,
 			name: 'طراحی پوستر فروشگاه send shoes',
 			slug: 'poster-4',
+			category: 'design',
 			posterSrc: poster_4,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -133,6 +147,7 @@ export const projects = {
 			id: 1,
 			name: 'طراحی بک گراند شهادت امام کاظم (ع)',
 			slug: 'mazhabi-1',
+			category: 'design',
 			posterSrc: mazhabi_1,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -140,6 +155,7 @@ export const projects = {
 			id: 2,
 			name: 'طراحی پوستر ایام فاطمیه',
 			slug: 'mazhabi-2',
+			category: 'design',
 			posterSrc: mazhabi_2,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -147,6 +163,7 @@ export const projects = {
 			id: 3,
 			name: 'طراحی کاور پست',
 			slug: 'mazhabi-3',
+			category: 'design',
 			posterSrc: mazhabi_3,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -154,6 +171,7 @@ export const projects = {
 			id: 4,
 			name: 'طراحی پوستر هیئت هفتگی',
 			slug: 'mazhabi-4',
+			category: 'design',
 			posterSrc: mazhabi_4,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -161,6 +179,7 @@ export const projects = {
 			id: 5,
 			name: 'طراحی پست',
 			slug: 'mazhabi-5',
+			category: 'design',
 			posterSrc: mazhabi_5,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -168,6 +187,7 @@ export const projects = {
 			id: 6,
 			name: 'طراحی بک گراند ولادت امام زمان (عج)',
 			slug: 'mazhabi-6',
+			category: 'design',
 			posterSrc: mazhabi_6,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -175,6 +195,7 @@ export const projects = {
 			id: 7,
 			name: 'طراحی لوگو صوت سما',
 			slug: 'mazhabi-6',
+			category: 'design',
 			posterSrc: logo_1,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},
@@ -182,6 +203,7 @@ export const projects = {
 			id: 8,
 			name: 'طراحی عکس هنری',
 			slug: 'mazhabi-6',
+			category: 'design',
 			posterSrc: artistic_photo_1,
 			videoSrc: 'https://1drv.ms/v/s!ArNeogtEVeNZiwMhB062NMWqeU6u?e=LDhUtI',
 		},

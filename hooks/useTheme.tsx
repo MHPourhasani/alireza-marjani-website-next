f3c7f9eac3afa582f99d7a6f7fd8d2fced001183
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-10-03 18:04:21 +0330

    create useTheme hook

---
+++ hooks/useTheme.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { PropsWithChildren, createContext, useContext, useState } from 'react';
+
+type Themes = 'dark' | 'light';
+type ThemeState = {
+    theme: Themes;
+    setTheme(theme: Themes): void;
+};
+
+const ThemeContext = createContext<ThemeState | null>(null);
+
+const useTheme = (): ThemeState => {
+    const context = useContext(ThemeContext);
+
+    if (!context) {
+        throw new Error('Please use ThemeProvider in parent component');
+    }
+
+    return context;
+};
+
+export const ThemeProvider = (props: PropsWithChildren) => {
+    const [theme, setTheme] = useState<Themes>('dark');
+
+    return <ThemeContext.Provider value={{ theme, setTheme }}>{props.children}</ThemeContext.Provider>;
+};
+
+export default useTheme;

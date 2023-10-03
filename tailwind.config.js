488992b0771313938a6e4b8ca420e6bf906c4067
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-10-03 17:17:30 +0330

    update tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: "class",
-	content: ['./src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		fontFamily: {
-			iranyekan: ['iranyekan'],
-		},
-		extend: {
-			colors: {
-				bgColor: '#ECF0F3',
-				darkBgColor: '#21252A',
-				primary: '#FD014F',
-				secondary: '#838485',
-				gradientOne: '#EFF3F6',
-				gradientTwo: '#ffffff',
-				darkGradientOne: '#161A20',
-				darkGradientTwo: '#23272C',
-			},
-		},
-	},
-	plugins: [],
+    darkMode: 'class',
+    content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
+    theme: {
+        fontFamily: {
+            iranyekan: ['iranyekan'],
+        },
+        extend: {
+            colors: {
+                bgColor: '#ECF0F3',
+                darkBgColor: '#21252A',
+                primary: '#FD014F',
+                secondary: '#838485',
+                gradientOne: '#EFF3F6',
+                gradientTwo: '#ffffff',
+                darkGradientOne: '#161A20',
+                darkGradientTwo: '#23272C',
+            },
+        },
+    },
+    plugins: [],
 };
